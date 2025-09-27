1b1cacdd6c1f099fc2c8f8cb47e939c82eea64e8
massimilianoforestiero <maxi941@hotmail.it>
2025-09-27 22:14:52 +0200

    Add deployment workflow for Droplet using GitHub Actions

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 
+// Serve i file statici della build di React
+app.use(express.static(path.join(__dirname, '../build')));
+
+// API example
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Sito in costruzione' });
+});
+
+// Per tutte le altre route, ritorna index.html (React Router)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 4000;
