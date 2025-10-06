9e09cccdea5068ca255b7890fb6f5a78249eb345
massimilianoforestiero <maxi941@hotmail.it>
2025-10-06 22:06:57 +0200

    Add responsive navigation bar with hamburger menu and social links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,8 @@
-import {NavBar} from '../components/NavBar/NavBar';
+import {NavBar} from './NavBar/NavBar';
 
 export function Layout({ children }) {
   return (
-    <div style={{ maxWidth: 800, margin: '0 auto', fontFamily: 'sans-serif' }}>
+      <div style={{width: '100%'}}>
         <NavBar/>
       <main>{children}</main>
     </div>
